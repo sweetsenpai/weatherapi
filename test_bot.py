b56f9d14f3c57e9cc92c7a2b1d07a1c76077bc06
Sasha <chcolatemilk00@gmail.com>
2019-02-28 21:45:10 +0400

    realize money finction and some other stuff

--- test_bot.py
+++ test_bot.py
@@ -1,17 +1,20 @@
 import time
 import telebot
 from telebot import apihelper
-
+import money
+#  from weatherapi import weather
 apihelper.proxy = {'https': 'socks5://352354383:RiqvhK6t@phobos.public.opennetwork.cc:1090'}
 TOKEN = "559015083:AAFmBW3TV6NEX579WlMEmgDczsuLekDxPIg"
 bot = telebot.TeleBot(token=TOKEN)
 
 
-def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
+"""
+def findw(msg):
+    # from a list of texts, it finds the one with the 'погода' sign
     for i in msg:
-        if '@' in i:
+        if 'погода' in i:
             return i
+"""
 
 
 @bot.message_handler(commands=['start'])  # welcome message handler
@@ -30,20 +33,43 @@ def send_msg(message):
     bot.send_message(message.chat.id, '😉\ncreated by: @Sweet_Sempai\n github: https://github.com/sweetsenpai')
 
 
+@bot.message_handler(commands=['weather☁'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Функция на доработке')
+
+
 @bot.message_handler(commands=['buttons'])
 def buttons_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row('/start', '/help', '/info')
-    user_markup.row('погода☁')
-    bot.send_message(message.chat.id, "Лови кнопки приятель🔳", reply_markup=user_markup)
+    user_markup.row('/weather☁', '/news📰', '/money💰')
+    bot.send_message(message.chat.id, "Лови кнопки приятель⚙️", reply_markup=user_markup)
+
+"""
+@bot.message_handler(func=lambda msg: msg.text is not None and 'погода' or 'Погода' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def find_city(message):
+    texts = message.text.split()
+    at_text = findw(texts)
+    bot.reply_to(message, weather(texts[-1]))
+"""
 
 
-@bot.message_handler(content_types=['text'])
-def handle_msg(message):
-    if message.text == 'погода☁':
-        bot.send_message(message.chat.id, 'Совсем скоро эта функция будет добавлена😁')
-    else:
-        bot.send_message(message.chat.id, 'Я пока не знаю что с этим делать😢')
+@bot.message_handler(commands=['money💰'])
+def get_money(message):
+    bot.send_message(message.chat.id, money.money_status())
+
+
+@bot.message_handler(commands=['news📰'])
+def inline(message):
+    key = telebot.types.InlineKeyboardMarkup(True,)
+    but_1 = telebot.types.InlineKeyboardButton(text='Медуза', url='https://meduza.io')
+    but_2 = telebot.types.InlineKeyboardButton(text='МедиаЗона', url='https://zona.media')
+    but_3 = telebot.types.InlineKeyboardButton(text='Лента.ру', url='https://lenta.ru')
+    key.row(but_1, but_2, but_3)
+    bot.send_message(message.chat.id, "НОВОСТИ", reply_markup=key)
+
 
 
 while True:
--- weatherapi.py
+++ weatherapi.py
@@ -1,29 +1,35 @@
-#import requests
-#from pprint import pprint
-from googletrans import Translator
-translator = Translator()
-print(translator.translate('Very cold here.'))
-"""
-city = input('Enter your city : ')
+import requests
+from pprint import pprint
+import re
+from bs4 import BeautifulSoup as bs
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ff4138355d772de3d5e02734eea46925'.format(city)
 
-res = requests.get(url)
+def weather(town):
+    city = town
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ff4138355d772de3d5e02734eea46925'.format(city)
 
-data = res.json()
-print(data)
-
-temp = data['main']['temp']
-wind_speed = data['wind']['speed']
-
-latitude = data['coord']['lat']
-longitude = data['coord']['lon']
-
-description = data['weather'][0]['description']
-
-print('Temperature : {} degree celcius'.format(temp))
-print('Wind Speed : {} m/s'.format(wind_speed))
-print('Latitude : {}'.format(latitude))
-print('Longitude : {}'.format(longitude))
-print('Description : {}'.format(description))
-"""
+    headers = {
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.65'
+    }
+    session = requests.Session()
+    ask = session.get(url, headers=headers)
+    if ask.status_code == 404:
+        return ('Возникли какие-то проблемы😱,\nпроверти правильно ли вы ввели название вашего города.\n'
+                'Если все верно,пожалуйста введите название вашего города латинецей.')
+    res = requests.get(url)
+    data = res.json()
+    #print(data)
+    temp = data['main']['temp']
+    wind_speed = data['wind']['speed']
+    degrees = int(temp - 273.15)
+    description = data['weather'][0]['description']
+    if degrees <= 0:
+        a =('Температура : ' + str(degrees) + '°' + "❄\n")
+    elif degrees < 10:
+        a = ('Температура : ' + str(degrees) + '°' + ' 👌\n')
+    elif degrees > 10:
+        a = ('Температура : ' + str(degrees) + '°' + '☀\n')
+    a = a + ('Скорость ветра 🌬️: {} м/с'.format(wind_speed))
+    return a
